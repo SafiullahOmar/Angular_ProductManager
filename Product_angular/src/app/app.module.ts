893a3ae6e69cdf0afd7d9544e9a73871d148d171
Safiullah Omar <omar.safiullah@gmail.com>
2022-07-23 08:53:45 +0430

    login features and code

--- Product_angular/src/app/app.module.ts
+++ Product_angular/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RegisterComponent } from './register/register.component';
 import { ProductsComponent } from './products/products.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Product_angular/src/app/login/login.component.ts
+++ Product_angular/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AccountServicesService } from '../service/account-services.service';
 
 @Component({
@@ -9,12 +10,36 @@ import { AccountServicesService } from '../service/account-services.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private account: AccountServicesService) { }
-  inserForm!: FormGroup;
+  constructor(private account: AccountServicesService,private router:Router,private activatedrt:ActivatedRoute,private fb:FormBuilder) { }
+  insertForm!: FormGroup;
   UserName!: FormControl;
   Password!: FormControl;
   returnURL!: string;
+  ErrorMessage!:string;
+  invalidLogin!:boolean;
   ngOnInit(): void {
+    this.UserName=new FormControl('',[Validators.required]);
+    this.Password=new FormControl('',[Validators.required]);
+    this.returnURL=this.activatedrt.snapshot.queryParams['returnURL']||'/';
+    this.insertForm=this.fb.group({
+      "UserName":this.UserName,
+      "Password":this.Password
+    });
+  }
+
+  onSubmit(){
+    let userLogin=this.insertForm.value;
+    this.account.login(userLogin.UserName,userLogin.Password).subscribe((res)=>{
+      let token=(<any>res).token;
+      console.log('login');
+      this.invalidLogin=false;
+      this.router.navigateByUrl(this.returnURL);
+
+    },(error)=>{
+
+      this.invalidLogin=true;
+      this.ErrorMessage="invalid details";
+    });    
   }
 
 }
