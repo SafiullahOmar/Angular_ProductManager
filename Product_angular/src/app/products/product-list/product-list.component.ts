18f7e510e57669cacac1e9cc583afb9a57ffe7ed
Safiullah Omar <omar.safiullah@gmail.com>
2022-07-26 20:05:04 +0430

    datatable installation & product list insert,updatet modals

--- Product_angular/src/app/products/product-list/product-list.component.ts
+++ Product_angular/src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
@@ -7,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ProductListComponent implements OnInit {
+
+insertForm!:FormGroup;
+name?:FormControl;
+price?:FormControl;
+description?:FormControl;
+imgURL?:FormControl;
+
+
+updateForm?:FormGroup;
+_name?:FormControl;
+_price?:FormControl;
+_description?:FormControl;
+_imgURL?:FormControl;
+_id?:FormControl;
 
   constructor() { }
 
