2d30df5f590dc4cff3c73a5400cc99cdb207851d
Safiullah Omar <omar.safiullah@gmail.com>
2022-07-27 18:09:17 +0430

    product service auth

--- Product_angular/src/app/products/product-list/product-list.component.ts
+++ Product_angular/src/app/products/product-list/product-list.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { DataTableDirective } from 'angular-datatables';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { Subject, subscribeOn } from 'rxjs';
+import { Observable } from 'rxjs';
+import { product } from 'src/app/service/product';
+import { ProductService } from 'src/app/service/product.service';
+
 
 @Component({
   selector: 'app-product-list',
@@ -15,7 +23,7 @@ price?:FormControl;
 description?:FormControl;
 imgURL?:FormControl;
 
-
+//update 
 updateForm?:FormGroup;
 _name?:FormControl;
 _price?:FormControl;
@@ -23,9 +31,41 @@ _description?:FormControl;
 _imgURL?:FormControl;
 _id?:FormControl;
 
-  constructor() { }
+//add modal
+
+@ViewChild('template') modal!:TemplateRef<any>;
+
+// update
+
+@ViewChild('editTemplate') editmodal!:TemplateRef<any>;
+
+//modal properties
+modalMessage?:string;
+modalRef!:BsModalRef;
+selectedProduct?:product;
+product$!:Observable<product[]>;
+products:product[]=[];
+userRoleStatus?:string;
+
+dtOptions: DataTables.Settings = {};
+dtTrigger:Subject<any>=new Subject();
+
+@ViewChild(DataTableDirective) dtElement!:DataTableDirective;
+  constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
+     this.dtOptions={
+      pagingType:'full _numbers',
+      pageLength:5,
+      autoWidth:true,
+      order:[[0,'desc']]
+     }
+
+     this.product$=this.productService.getproducts();
+     this.product$.subscribe(res=>{
+      this.products=res;
+      this.dtTrigger.next(this.products);
+     });
   }
 
 }
